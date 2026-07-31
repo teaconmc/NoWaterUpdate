f7078d1821b7ad92b3bab75acdafbcacbd029968
USS-Shenzhou <969840795@qq.com>
2026-07-31 20:53:57 +0800

    limit to water

--- src/main/java/cn/ussshenzhou/nowaterupdate/mixin/FlowingFluidMixin.java
+++ src/main/java/cn/ussshenzhou/nowaterupdate/mixin/FlowingFluidMixin.java
@@ -4,10 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.material.FlowingFluid;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.WaterFluid;
+import net.minecraft.world.level.material.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,10 +16,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(FlowingFluid.class)
 public class FlowingFluidMixin {
 
+    @SuppressWarnings("ConstantValue")
     @Inject(method = "canMaybePassThrough", at = @At("HEAD"), cancellable = true)
     private void noWaterUpdateDisableWaterUpdate(BlockGetter level, BlockPos sourcePos, BlockState sourceState, Direction direction, BlockPos testPos, BlockState testState, FluidState testFluidState, CallbackInfoReturnable<Boolean> cir) {
-        if ((Object) this instanceof WaterFluid) {
+        if ((Object) this == Fluids.WATER || (Object) this == Fluids.FLOWING_WATER) {
             cir.setReturnValue(false);
         }
+        //if ((Object) this instanceof WaterFluid) {
+        //    cir.setReturnValue(false);
+        //}
     }
 }
